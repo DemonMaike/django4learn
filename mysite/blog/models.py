93a2165dc740901b3f0d54e739fdfc55acd98469
Dmitry Malyshev <dd89268147556@gmail.com>
2024-06-21 22:18:07 +0400

    Update

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,18 +1,30 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
-    
+
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
         PUBLISHED = "PB", "Published"
-    
+
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
-                             on_delete=models.CASCADE,
-                             related_name="blog_posts")
+                               on_delete=models.CASCADE,
+                               related_name="blog_posts")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -30,4 +42,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
