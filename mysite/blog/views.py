4a44b6ddcbae826768bf2f6da8cea81243381542
Dmitry Malyshev <dd89268147556@gmail.com>
2024-07-10 20:31:59 +0400

    Update blog. Added Search

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,8 @@ from .models import Post
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .forms import CommentForm, EmailPostForm
+from django.contrib.postgres.search import SearchVector
+from .forms import CommentForm, EmailPostForm, SearchForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 
@@ -12,6 +13,25 @@ from taggit.models import Tag
 #     context_object_name = 'posts'
 #     paginate_by = 3
 #     template_name = 'blog/post/list.html'
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search = SearchVector('title', 'body')
+            ).filter(search=query)
+
+    return render(request, 'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
+
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
